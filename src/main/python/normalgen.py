b2762f6d0874d988cbf06157e86728a45d7e948d
leszek.marcjan <leszek.marcjan@iqor.pl>
2020-12-04 13:31:35 +0100

    requirements.txt

--- src/main/python/normalgen.py
+++ src/main/python/normalgen.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-x = np.random.normal(size=100, loc=50)
-y = [sum([np.random.rand() for j in range(1, 100)]) for i in range(1, 100)]
+if __name__ == '__main__':
 
-sns.distplot(x)
-sns.distplot(y)
+    x = np.random.normal(size=100, loc=50)
+    y = [sum([np.random.rand() for j in range(1, 100)]) for i in range(1, 100)]
 
-plt.show()
+    sns.distplot(x)
+    sns.distplot(y)
+
+    plt.show()
