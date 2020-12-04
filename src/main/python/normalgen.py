b0f919d9620973890d616a46ff7c0c7d70f262e4
leszek.marcjan <leszek.marcjan@iqor.pl>
2020-12-04 14:05:54 +0100

    better plot

--- src/main/python/normalgen.py
+++ src/main/python/normalgen.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 if __name__ == '__main__':
+    df = pd.DataFrame({
+        'Normal': np.random.normal(size=100, loc=50),
+        'Generated': [sum([np.random.rand() for j in range(0, 100)]) for i in range(0, 100)]
+    })
 
-    x = np.random.normal(size=100, loc=50)
-    y = [sum([np.random.rand() for j in range(1, 100)]) for i in range(1, 100)]
-
-    sns.distplot(x)
-    sns.distplot(y)
+    sns.displot(df, kind="kde", fill=True)
 
     plt.show()
