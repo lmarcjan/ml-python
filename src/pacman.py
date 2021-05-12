dd3929a75a7b43fdfffcaceeb3173d6ad0799655
leszek.marcjan <leszek.marcjan@iqor.com>
2021-05-12 22:07:11 +0200

    stabilization

--- src/pacman.py
+++ src/pacman.py
@@ -1,11 +1,9 @@
 import gym
-import matplotlib.pyplot as plt
 
 from util.plot_util import plot_animation
 
 frames = []
 n_max_steps = 1000
-n_change_steps = 10
 
 if __name__ == '__main__':
     env = gym.make('MsPacman-v0')
@@ -13,11 +11,10 @@ if __name__ == '__main__':
     for step in range(n_max_steps):
         img = env.render(mode="rgb_array")
         frames.append(img)
-        if step % n_change_steps == 0:
-            action = env.action_space.sample()
+        action = env.action_space.sample()
         state, reward, done, _ = env.step(action)
         if done:
             break
 
-    video = plot_animation(frames)
-    plt.show()
+    plot_animation(frames)
+
