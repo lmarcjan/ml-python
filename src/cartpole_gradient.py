4b9bded9f2ac70f76fe4bd51cda0dad2e44ae752
Leszek Marcjan <leszek.marcjan@iqor.com>
2021-06-01 13:28:46 +0200

    ms pacman

--- src/cartpole_gradient.py
+++ src/cartpole_gradient.py
@@ -5,11 +5,6 @@ tf.disable_v2_behavior()
 
 env = gym.make("CartPole-v0")
 
-n_games_per_update = 10
-n_max_steps = 1000
-n_episode = 250
-discount_rate = 0.95
-
 
 def render_policy(model_path, action, X, n_max_steps=1000):
     state = env.reset()
@@ -24,17 +19,17 @@ def render_policy(model_path, action, X, n_max_steps=1000):
     env.close()
 
 
-def discount_rewards(rewards, discount_rate):
+def discount_rewards(rewards, gamma):
     discounted_rewards = np.zeros(len(rewards))
     cumulative_rewards = 0
     for step in reversed(range(len(rewards))):
-        cumulative_rewards = rewards[step] + cumulative_rewards * discount_rate
+        cumulative_rewards = rewards[step] + cumulative_rewards * gamma
         discounted_rewards[step] = cumulative_rewards
     return discounted_rewards
 
 
-def discount_and_normalize_rewards(all_rewards, discount_rate):
-    all_discounted_rewards = [discount_rewards(rewards, discount_rate) for rewards in all_rewards]
+def discount_and_normalize_rewards(all_rewards, gamma):
+    all_discounted_rewards = [discount_rewards(rewards, gamma) for rewards in all_rewards]
     flat_rewards = np.concatenate(all_discounted_rewards)
     reward_mean = flat_rewards.mean()
     reward_std = flat_rewards.std()
@@ -46,6 +41,10 @@ if __name__ == '__main__':
     n_hidden = 4
     n_outputs = 1
     learning_rate = 0.01
+    n_games_per_update = 10
+    n_max_steps = 1000
+    n_episode = 250
+    gamma = 0.95
 
     initializer = tf.variance_scaling_initializer()
     X = tf.placeholder(tf.float32, shape=[None, n_inputs])
@@ -92,7 +91,7 @@ if __name__ == '__main__':
                 all_rewards.append(current_rewards)
                 all_gradients.append(current_gradients)
 
-            all_rewards = discount_and_normalize_rewards(all_rewards, discount_rate=discount_rate)
+            all_rewards = discount_and_normalize_rewards(all_rewards, gamma=gamma)
             feed_dict = {}
             for var_index, gradient_placeholder in enumerate(gradient_placeholders):
                 mean_gradients = np.mean([reward * all_gradients[game_index][step][var_index]
