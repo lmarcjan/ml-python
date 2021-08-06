552e1a03f4bfa0ccc8262ed61b8793b1ce38d90b
leszek.marcjan <leszek.marcjan@iqor.com>
2021-08-06 17:47:13 +0200

    stabilization

--- src/util/stat_util.py
+++ src/util/stat_util.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
 from scipy.stats import spearmanr
 import pandas as pd
-from sklearn.metrics import f1_score, recall_score, precision_score
+from sklearn.metrics import f1_score, recall_score, precision_score, confusion_matrix, classification_report
 
 
 def predict_error(model, X, y, prefix=""):
@@ -14,11 +14,15 @@ def predict_error(model, X, y, prefix=""):
     print(f'{prefix} Root Mean Squared Error:', round(np.sqrt(metrics.mean_squared_error(y, y_pred)), 4))
 
 
-def predict_score(model, X, y, prefix=""):
-    y_pred = model.predict(X)
+def predict_score(clf, X, y, prefix=""):
+    y_pred = clf.predict(X)
     print(f'{prefix} Precision Score:', round(precision_score(y, y_pred, average='weighted'), 4))
     print(f'{prefix} Recall Score:', round(recall_score(y, y_pred, average='weighted'), 4))
     print(f'{prefix} F1 Score:', round(f1_score(y, y_pred, average='weighted'), 4))
+    cm = confusion_matrix(y, y_pred)
+    print(f'{prefix} Confusion Matrix:\n', cm)
+    cr = classification_report(y, y_pred)
+    print(f'{prefix} Classification Report:\n', cr)
 
 
 def plot_corr(df, columns):
