f96439e2a0ba8d4949d20c11a20b1ca2d985f3d0
leszek.marcjan <leszek.marcjan@iqor.pl>
2023-10-31 16:07:50 +0100

    Fix.

--- src/text_generation.py
+++ src/text_generation.py
@@ -1,14 +1,12 @@
-import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model_name = "gpt2"
 
 if __name__ == '__main__':
 
-    torch_device = "cuda" if torch.cuda.is_available() else "cpu"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
-    text = "What is love?"
-    encoded_input = tokenizer(text, return_tensors='pt')
-    output = model(**encoded_input)
-    print("Output:\n\n"+tokenizer.decode(output.logits[0], skip_special_tokens=True))
+    input = "What is AI?"
+    encoded_input = tokenizer.encode(input, return_tensors='pt')
+    output = model.generate(encoded_input, max_length=200, do_sample=True, num_beams=5, no_repeat_ngram_size=2, early_stopping=True)
+    print("Output:\n\n"+tokenizer.decode(output[0], skip_special_tokens=True))
