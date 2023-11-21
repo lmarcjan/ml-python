642c739d717262402628439941f7c9854a13a37a
leszek <leszek.marcjan@gmail.com>
2023-11-21 15:40:30 +0100

    Fix.

--- src/text_generation.py
+++ src/text_generation.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     model = GPT2LMHeadModel.from_pretrained(model_name)
-    input = "What is AI?"
-    encoded_input = tokenizer.encode(input, return_tensors='pt')
-    output = model.generate(encoded_input, max_length=200, do_sample=True)
+    query = "What is AI?"
+    inputs = tokenizer.encode(query, return_tensors='pt')
+    output = model.generate(inputs, max_length=200, do_sample=True)
     print("Output:\n\n"+tokenizer.decode(output[0], skip_special_tokens=True))
